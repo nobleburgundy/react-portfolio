237136a4b7d9674275c1c1f547800c93f919276d
James Gould <nobleburgundy@gmail.com>
2021-03-09 10:19:02 -0600

    Style and content updates to Home and About pages

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import PageContainer from "../../components/PageContainer";
 
 function About() {
+  const aboutImageSrc = "https://james-gould.herokuapp.com/assets/img/personal/piano.jpg";
   return (
     <>
       <PageContainer>
         <h1>About</h1>
+        <img className="w-100" src={aboutImageSrc} alt="About" />
       </PageContainer>
     </>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PageContainer from "../../components/PageContainer";
+import "./style.css";
+const portfolioImageSrc = "https://james-gould.herokuapp.com/assets/img/personal/PortfolioMe-5.jpg";
 
 function Home() {
   return (
     <>
       <PageContainer>
-        <img className="portfolio-image w-100" src="https://placehold.it/500x500" alt="Home page link" />
+        <img className="profile-image w-100" src={portfolioImageSrc} alt="Home page link" />
         <h1>JCG DEV</h1>
       </PageContainer>
     </>
