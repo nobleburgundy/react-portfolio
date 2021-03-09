5d7f7b606e37076f701fd5117dbb26a5cb3d0323
James Gould <nobleburgundy@gmail.com>
2021-03-09 09:57:19 -0600

    Imported bootstrap js and added navbar toggle

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,17 +4,41 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/portfolio">Portfolio</Link>
-        </li>
-      </ul>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">
+          James Gould
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navLinks"
+          aria-controls="#navLinks"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navLinks">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/portfolio">
+                Portfolio
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 }
