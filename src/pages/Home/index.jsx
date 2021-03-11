b137b29062f06981516188dca1ce4bf76f2abcac
James Gould <nobleburgundy@gmail.com>
2021-03-10 21:25:21 -0600

    Changed pic on home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import PageContainer from "../../components/PageContainer";
 // import api from "../../utils/api";
 import "./style.css";
-const portfolioImageSrc = "https://james-gould.herokuapp.com/assets/img/personal/PortfolioMe-5.jpg";
+import Pic from "./stage.jpg";
 
 function Home() {
   return (
     <>
       <PageContainer>
         <h2 className="text-center text-monospace">Musician/Engineer</h2>
-        <img className="profile-image w-100" src={portfolioImageSrc} alt="Home page link" />
+        <img className="profile-image w-100" src={Pic} alt="Home page link" />
       </PageContainer>
     </>
   );
